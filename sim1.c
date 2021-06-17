a1d82913d0d52841b30595a643c88274a148f8c4
Orlando Rodriguez <46053650+OrlieRod314@users.noreply.github.com>
2021-06-17 15:18:44 -0700

    Add files via upload

--- sim1.c
+++ sim1.c
@@ -8,19 +8,19 @@ void execute_add(Sim1Data *obj)
 {
 	// TODO: implement me!
 	int carryIn = 0;
-	int carryOut = obj.isSubtraction;
+	int carryOut = obj->isSubtraction;
 
-	obj.aNonNeg = ~((obj.a >> 31) & 0x1);
-	obj.bNonNeg = ~((obj.b >> 31) & 0x1);
+	obj->aNonNeg = ~((obj->a >> 31) & 0x1);
+	obj->bNonNeg = ~((obj->b >> 31) & 0x1);
 
 	for (int i = 0; i < 32; i++) {
         carryIn = carryOut;
-        int tempa = (obj.a >> i) & 0x1;
-        int tempb = (obj.b >> i) & 0x1;
-        if (obj.isSubtraction == 1)
+        int tempa = (obj->a >> i) & 0x1;
+        int tempb = (obj->b >> i) & 0x1;
+        if (obj->isSubtraction == 1)
             tempb = ~tempb;
         if (carryIn ^ (tempa ^ tempb) == 1)
-            obj.sum |= (0x1 << i);
+            obj->sum |= (0x1 << i);
 
         int XOR1 = (tempa ^ tempb);
         int AND1 = (XOR1 & carry);
@@ -28,7 +28,7 @@ void execute_add(Sim1Data *obj)
         carryOut = (AND1 ^ AND2);
 	}
 
-    obj.carryOut = carryOut;
-    obj.sumNonNeg = ~((obj.sum >> 31) & 0x1);
-    obj.overflow = (carryIn ^ carryOut);
+    obj->carryOut = carryOut;
+    obj->sumNonNeg = ~((obj->sum >> 31) & 0x1);
+    obj->overflow = (carryIn ^ carryOut);
 }
