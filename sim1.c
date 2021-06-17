a642f1c6e1a9ba45871564b81e27866bbca75e87
Orlando Rodriguez <46053650+OrlieRod314@users.noreply.github.com>
2021-06-17 16:27:52 -0700

    Add files via upload

--- sim1.c
+++ sim1.c
@@ -4,24 +4,32 @@
  */
 #include "sim1.h"
 
-void execute_add(Sim1Data *obj)
-{
-	// TODO: implement me!
+void execute_add(Sim1Data *obj) {
+	// Want to set temporary carry variables
 	int carryIn = 0;
-	int carryOut = obj->isSubtraction;
+	int carryOut = obj->isSubtraction; // Convenient value, so that it's adopted by carryIn on first iteration
 
+	// Check if a and b are non negative
+	// You isolate the MSB, negate it, and isolate it again so you have either 00...01 or 00...00
 	obj->aNonNeg = ~((obj->a >> 31) & 0x1) & 0x1;
 	obj->bNonNeg = ~((obj->b >> 31) & 0x1) & 0x1;
 
 	for (int i = 0; i < 32; i++) {
+        // Old carryOut is now carried in
         carryIn = carryOut;
+        // Isolating corresponding bits
         int tempa = (obj->a >> i) & 0x1;
         int tempb = (obj->b >> i) & 0x1;
+
+        // Reversing tempb if it is a subtraction
         if (obj->isSubtraction == 1)
             tempb = ~tempb & 0x1;
-        if (carryIn ^ (tempa ^ tempb) == 1)
-            obj->sum |= (0x1 << i);
 
+        // Updating the right sum value
+        if (carryIn ^ (tempa ^ tempb) == 1) // Sum is by default 0x0, so it is not changed if a bit doesn't need to be flipped
+            obj->sum |= (0x1 << i); // Have to shift left to align with the correct column
+
+        // Separate carryOut calculation
         int XOR1 = (tempa ^ tempb);
         int AND1 = (XOR1 & carryIn);
         int AND2 = (tempa & tempb);
@@ -30,5 +38,5 @@ void execute_add(Sim1Data *obj)
 
     obj->carryOut = carryOut;
     obj->sumNonNeg = ~((obj->sum >> 31) & 0x1) & 0x1;
-    obj->overflow = (carryIn ^ carryOut);
+    obj->overflow = (carryIn ^ carryOut); // Use XOR with carryIn and carryOut of MSB
 }
