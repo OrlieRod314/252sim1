60e297603368b934ee1ca6138bf34f377d124454
Orlando Rodriguez <46053650+OrlieRod314@users.noreply.github.com>
2021-06-17 15:10:04 -0700

    Add files via upload

--- sim1.c
+++ sim1.c
@@ -1,15 +1,34 @@
 /* Implementation of a 32-bit adder in C.
  *
- * Author: TODO
+ * Author: Orlando Rodriguez
  */
-
-
 #include "sim1.h"
-
-
 
 void execute_add(Sim1Data *obj)
 {
 	// TODO: implement me!
-}
+	int carryIn = 0;
+	int carryOut = obj.isSubtraction;
 
+	obj.aNonNeg = ~((obj.a >> 31) & 0x1);
+	obj.bNonNeg = ~((obj.b >> 31) & 0x1);
+
+	for (int i = 0; i < 32; i++) {
+        carryIn = carryOut;
+        int tempa = (obj.a >> i) & 0x1;
+        int tempb = (obj.b >> i) & 0x1;
+        if (obj.isSubtraction == 1)
+            tempb = ~tempb;
+        if (carryIn ^ (tempa ^ tempb) == 1)
+            obj.sum |= (0x1 << i);
+
+        int XOR1 = (tempa ^ tempb);
+        int AND1 = (XOR1 & carry);
+        int AND2 = (tempa & tempb);
+        carryOut = (AND1 ^ AND2);
+	}
+
+    obj.carryOut = carryOut;
+    obj.sumNonNeg = ~((obj.sum >> 31) & 0x1);
+    obj.overflow = (carryIn ^ carryOut);
+}
